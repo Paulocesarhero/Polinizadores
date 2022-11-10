547f701c38df65307664572c84594dfb6458da04
Victor <cuevas0122@gmail.com>
2022-11-10 14:37:47 -0600

    Lógica de ventana implementada

--- src/main/java/uv/fei/polinizadores/Publicaciones.java
+++ src/main/java/uv/fei/polinizadores/Publicaciones.java
@@ -1,19 +1,73 @@
 package uv.fei.polinizadores;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import uv.fei.bussinesslogic.PublicacionDAO;
+import uv.fei.domain.Publicacion;
 
-public class Publicaciones {
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Publicaciones implements Initializable {
     public ComboBox comboEstado;
-    public TableView tablaPublicaciones;
-    public TableColumn columnaNombretable;
-    public TableColumn columnaId;
-    public TableColumn columnaEstado;
+    public TableView<Publicacion> tablaPublicaciones;
+    public TableColumn<Publicacion, String> columnaNombretable;
+    public TableColumn<Publicacion, Integer> columnaId;
+    public TableColumn<Publicacion, CheckBox> columnaEstado;
     public Button btnGuardar;
+    private List<Publicacion> listaPublicaciones = new ArrayList();
 
-    public void clicGuardar(ActionEvent actionEvent) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        llenarListaPublicaciones();
+        llenarTabla();
     }
+    public void clicGuardar(ActionEvent actionEvent) {
+        boolean flag = false;
+        boolean message = true;
+        boolean unassignation = false;
+        String studentStaffNumber = "";
+        PublicacionDAO publicacionDAO = new PublicacionDAO();
+        Publicacion publicacion = new Publicacion();
+
+        for (int i = 0; i < tablaPublicaciones.getItems().size(); i++){
+            unassignation = tablaPublicaciones.getItems().get(i).isEstado();
+            publicacion = tablaPublicaciones.getSelectionModel().getSelectedItem();
+            //Metodo para guardar las publicaciones
+            try {
+                publicacionDAO.actualizarPublicacion(publicacion);
+            } catch (SQLException e) {
+                System.out.println("No se pudo actualizar la publicacion con id "+publicacion.getId());
+            }
+        }
+    }
+
+    private void llenarListaPublicaciones(){
+        PublicacionDAO publicacionDAO = new PublicacionDAO();
+
+        try {
+            listaPublicaciones = publicacionDAO.obtenerPublicaciones();
+        } catch (SQLException e) {
+            System.out.println("No se puede acceder a la base de datos");
+        }
+    }
+
+    private void llenarTabla(){
+        columnaId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnaNombretable.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+        columnaEstado.setCellValueFactory(new PropertyValueFactory<>("estado"));
+        columnaEstado.setEditable(true);
+
+        ObservableList<Publicacion> observableList = FXCollections.observableList(listaPublicaciones);
+        tablaPublicaciones.setItems(observableList);
+        tablaPublicaciones.setEditable(false);
+    }
+
 }
