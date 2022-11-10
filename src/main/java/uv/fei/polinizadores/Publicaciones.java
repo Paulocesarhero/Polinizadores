6d8b210f1a9abf21ecf72569796bcc3883209e31
Victor <cuevas0122@gmail.com>
2022-11-10 13:56:22 -0600

    Diseño de nueva ventana

--- src/main/java/uv/fei/polinizadores/Publicaciones.java
+++ src/main/java/uv/fei/polinizadores/Publicaciones.java
@@ -1,4 +1,19 @@
 package uv.fei.polinizadores;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class Publicaciones {
+    public ComboBox comboEstado;
+    public TableView tablaPublicaciones;
+    public TableColumn columnaNombretable;
+    public TableColumn columnaId;
+    public TableColumn columnaEstado;
+    public Button btnGuardar;
+
+    public void clicGuardar(ActionEvent actionEvent) {
+    }
 }
